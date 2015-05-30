26ed5969a10d79ebf7f325818068d805a123feb7
Uğurcan Laçin <lacinugur@hotmail.com.tr>
2015-05-31 00:13:38 +0300

    Add annotated classes to spring config file.

--- src/main/java/com/cyfres/model/Genre.java
+++ src/main/java/com/cyfres/model/Genre.java
@@ -1,36 +1,42 @@
 package com.cyfres.model;
 
-import static javax.persistence.GenerationType.IDENTITY;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "genre", catalog = "cyfres")
 public class Genre implements java.io.Serializable {
+
 	private Integer id;
 	private String name;
-	
+
+	public Genre() {
+	}
+
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
-	public int getId() {
-		return id;
+	public Integer getId() {
+		return this.id;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	@Column(name = "name", length = 255)
 	public String getName() {
-		return name;
+		return this.name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 }
