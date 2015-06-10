616f9203d80de1174d902c3af1a77e602f7294cd
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-06-10 23:02:53 +0300

    Added Session Control to Index Page

--- src/main/java/com/cyfres/controller/MainController.java
+++ src/main/java/com/cyfres/controller/MainController.java
@@ -1,4 +1,7 @@
 package com.cyfres.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +11,18 @@ import org.springframework.web.servlet.ModelAndView;
 public class MainController {
 	
 	@RequestMapping("/")
-	public ModelAndView home() {
-		ModelAndView model = new ModelAndView("login");
-		model.addObject("message", "Hi there!");
-		return model;
+	public ModelAndView home(HttpServletRequest request) {
+		boolean isLoggedIn = request.getSession().getAttribute("isLoggedIn") != null;
+		if (isLoggedIn) {
+			request.getSession().removeAttribute("isLoggedIn");
+			ModelAndView model = new ModelAndView("index");
+			model.addObject("message", isLoggedIn);
+			return model;
+		} else {
+			ModelAndView model = new ModelAndView("login");
+			model.addObject("message", isLoggedIn);
+			return model;
+		}
+		
 	}
 }
